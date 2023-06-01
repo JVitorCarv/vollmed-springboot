778b38adc6023c36fc73ad770435d882b54b1467
Vitor Carvalho <vitor.velozo2003@gmail.com>
2023-05-31 23:05:18 -0300

    First commit

---
+++ src/main/java/med/voll/api/controller/DadosCadastroPaciente.java
@@ -0,0 +1,12 @@
+package med.voll.api.controller;
+
+import med.voll.api.medico.DadosEndereco;
+
+public record DadosCadastroPaciente(
+        String nome,
+        String email,
+        String telefone,
+        String cpf,
+        DadosEndereco endereco
+) {
+}

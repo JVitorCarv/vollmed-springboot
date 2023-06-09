92360e4ecab4897381d81e4dcb4c7e80339c50cb
Vitor Carvalho <vitor.velozo2003@gmail.com>
2023-06-09 14:55:53 -0300

    Add and Delete medicos

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,7 +2,6 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.paciente.DadosCadastroPaciente;
 import med.voll.api.paciente.DadosListagemPaciente;
 import med.voll.api.paciente.Paciente;
